75e3c1f26bd0f07af4db2dbed448f4f9e58fdac7
Phil Robinson <philip.robinson84@gmail.com>
2016-08-19 21:09:36 +0100

    Added submission of temps

--- qsmartbrew.cpp
+++ qsmartbrew.cpp
@@ -2,6 +2,7 @@
 #include <qlabel.h>
 #include <qtimer.h>
 #include <QFile>
+#include <QTextStream>
 
 #include "qsmartbrew.h"
 
@@ -17,48 +18,77 @@ QSmartBrew::QSmartBrew(QWidget *parent)
 	startTimer(1000);
 }
 
+void QSmartBrew::sendTemperatures()
+{
+	// Setup the webservice url
+	QUrl serviceUrl = QUrl("http://philr.biz/smartbrew/record.php");
+	QByteArray postData;
+
+	QUrl params;
+	QUrlQuery query;
+	quint64 timeInt = QDateTime::currentMSecsSinceEpoch()
+	query.addQueryItem("deviceID","string1");
+	query.addQueryItem("tAmbient",m_tempAmb.toString());
+	query.addQueryItem("tFV",m_tempFV.toString());
+	query.addQueryItem("time",timeInt.toString());
+
+	params.setQuery(query);
+
+	postData = params.toEncoded(QUrl::RemoveFragment);
+
+	// Call the webservice
+	QNetworkAccessManager *networkManager = new QNetworkAccessManager(this);
+	connect(networkManager, SIGNAL(finished(QNetworkReply*)),
+	        SLOT(onPostAnswer(QNetworkReply*)));
+
+	QNetworkRequest networkRequest(serviceUrl);
+	networkRequest.setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
+
+	networkManager->post(networkRequest,postData);
+}
+
 void QSmartBrew::getTemperatures()
 {
 	// FV...
-	QFile inputFile = new QFile("/sys/bus/w1/devices/28-0216011c53ff/w1_slave");
-	if (inputFile.open(QIODevice::ReadOnly))
+	QFile inputFileFV("/sys/bus/w1/devices/28-0216011c53ff/w1_slave");
+	if (inputFileFV.open(QIODevice::ReadOnly))
 	{
-	   QTextStream in = new QTextStream(&inputFile);
-	   while (!in.atEnd())
+	   QTextStream inFV(&inputFileFV);
+	   while (!inFV.atEnd())
 	   {
-	      QString line = in.readLine();
+	      QString line = inFV.readLine();
 				QString isValid = line.right(4); // line contains "YES " if next line is valid temp
 
 				if (isValid.compare("YES ") == 0)
 				{
-					line = in.readLine();
+					line = inFV.readLine();
 					QString tempFVraw = line.right(6);
 					QString tempFVformatted = tempFVraw.left(2) + "." + tempFVraw.right(4);
 					m_tempFV = tempFVformatted.toFloat();
 				}
 	   }
-	   inputFile.close();
+	   inputFileFV.close();
 	}
 
 	// Amb...
-	QFile inputFile = new QFile("/sys/bus/w1/devices/28-021571c028ff/w1_slave");
-	if (inputFile.open(QIODevice::ReadOnly))
+	QFile inputFileAmb("/sys/bus/w1/devices/28-021571c028ff/w1_slave");
+	if (inputFileAmb.open(QIODevice::ReadOnly))
 	{
-		 QTextStream in = new QTextStream(&inputFile);
-		 while (!in.atEnd())
+		 QTextStream inAmb(&inputFileAmb);
+		 while (!inAmb.atEnd())
 		 {
-				QString line = in.readLine();
+				QString line = inAmb.readLine();
 				QString isValid = line.right(4); // line contains "YES " if next line is valid temp
 
 				if (isValid.compare("YES ") == 0)
 				{
-					line = in.readLine();
+					line = inAmb.readLine();
 					QString tempFVraw = line.right(6);
 					QString tempFVformatted = tempFVraw.left(2) + "." + tempFVraw.right(4);
 					m_tempFV = tempFVformatted.toFloat();
 				}
 		 }
-		 inputFile.close();
+		 inputFileAmb.close();
 	}
 }
 
@@ -66,7 +96,7 @@ void QSmartBrew::timerEvent(QTimerEvent *)
 {
 	m_timeout--;
 	getTemperatures();
-	//sendTemperatures();
+	sendTemperatures();
 
 	if (m_timeout < 1)
 		close();
@@ -93,7 +123,7 @@ void QSmartBrew::layoutWindow()
 	m_tempAmbLabel = new QLabel(QString::number(m_tempAmb, 'f', 3));
 	m_tempAmbLabel->setAlignment(Qt::AlignCenter);
 	m_tempAmbLabel->setStyleSheet("background-color: white; color: black");
-	layout->addWidget(m_tempAmb);
+	layout->addWidget(m_tempAmbLabel);
 
 	QWidget *widget = new QWidget;
 	widget->setLayout(layout);
